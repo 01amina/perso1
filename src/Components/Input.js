279f4a5cfd0d6f57e4805d24a7c1511fbef97dbb
Monssef <m-heroine@hotmail.com>
2023-01-26 13:25:00 +0800

    move Component to src/

---
+++ src/Components/Input.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  display: block;
+  width: 400px;
+  height: 40px;
+  border: 1px solid #ddd;
+  background-color: white;
+`;
